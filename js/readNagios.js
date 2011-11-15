d3c73d67420831b771f2a31b73a7db58b94cafbe
Andrew Beresford <a.j.beresford@sheffield.ac.uk>
2011-11-15 15:39:38 +0000

    Fix more parenthesis

--- js/readNagios.js
+++ js/readNagios.js
@@ -18,13 +18,13 @@ function updateGoogleCalendar() {
 
 	var out = '';
 
-	$.getJSON('http://www.google.com/calendar/feeds/sheffield.ac.uk_5i8cmsq79qd3nm5spk9ftkol0k@group.calendar.google.com/public/full?alt=json-in-script&callback=?&orderby=starttime&max-results=15&singleevents=true&sortorder=ascending&futureevents=true', 
-	  function (data) { $.each(data["feed"]["entry"], function(value, data) { 
-	    calDate = new Date(data["gd$when"][0]["startTime"]);
-	    dateString = calDate.getDate() + "/" + (calDate.getMonth()+1);
-	    $('#calendar').append("<tr><td class='date'>"+dateString+"</td><td class='event'>"+data["title"]["$t"]+"</td></tr>\n");
-	} 
-  )
+	$.getJSON('http://www.google.com/calendar/feeds/sheffield.ac.uk_5i8cmsq79qd3nm5spk9ftkol0k@group.calendar.google.com/public/full?alt=json-in-script&callback=?&orderby=starttime&max-results=15&singleevents=true&sortorder=ascending&futureevents=true', function (data) { 
+	  $.each(data["feed"]["entry"], function(value, data) { 
+	  calDate = new Date(data["gd$when"][0]["startTime"]);
+	  dateString = calDate.getDate() + "/" + (calDate.getMonth()+1);
+	  $('#calendar').append("<tr><td class='date'>"+dateString+"</td><td class='event'>"+data["title"]["$t"]+"</td></tr>\n");
+	})
+  })
 }
 
 updateGoogleCalendar();
