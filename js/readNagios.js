23d86b781dc7648f26c5f7a90c20a89e6a09dede
Andrew Beresford <a.j.beresford@sheffield.ac.uk>
2011-11-15 11:28:20 +0000

    use updateNagios function

---
+++ js/readNagios.js
@@ -0,0 +1,9 @@
+function updateNagios() {
+  $.getJSON('/vshell/index.php?type=services&mode=json', function(data) { $.each(data, function () { 
+    service = this["service_description"];
+    state = this["current_state"]; 
+    $("#service"+service).removeClass("OK CRITICAL ERROR WARNING"); 
+    $("#service"+service).addClass(state); 
+    console.debug(service+state); 
+  }) })
+}
