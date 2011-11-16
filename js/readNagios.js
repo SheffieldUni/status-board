85a0690436a9bf83848adda747d1e4e792a9382f
Andrew Beresford <a.j.beresford@sheffield.ac.uk>
2011-11-16 14:24:41 +0000

    more debug

--- js/readNagios.js
+++ js/readNagios.js
@@ -78,7 +78,7 @@ function updateGraphs() {
 	
     $.each(data, function () { 
       state[this["current_state"]]++;
-      console.debug( this["current_state"]);
+      console.debug( this["current_state"]+" - "+state[this["current_state"]] );
       if (state[this["current_state"]] > largestState) {
 	    largestState = state[this["current_state"]]; 
 	    console.debug(largestState)
