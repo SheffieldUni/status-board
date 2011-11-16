80657bce9a12cc654acd56912aea8d6bf07c01ca
Andrew Beresford <a.j.beresford@sheffield.ac.uk>
2011-11-16 12:32:38 +0000

    display correct text for next significant event

--- js/readNagios.js
+++ js/readNagios.js
@@ -24,10 +24,33 @@ function updateGoogleCalendar() {
   })
 }
 
+function updateSignificantEvent() {
+  $('#comingup').empty();
+
+  var out = '';
+
+  $.getJSON( 'http://www.google.com/calendar/feeds/sheffield.ac.uk_8v8avojjrlk1lot2lkpl47fn0k@group.calendar.google.com/public/full?alt=json-in-script&callback=?&orderby=starttime&max-results=1&singleevents=true&sortorder=ascending&futureevents=true', function (data) { 
+    entry = data["feed"]["entry"][0]
+    calDate = new Date(entry["gd$when"][0]["startTime"]);
+    calDate.setHours(0);
+    calDate.setMinutes(0);
+    calDate.setSeconds(0);
+    todayDate = new Date();
+    days = Math.floor((calDate - todayDate) / 1000 / 3600 / 24);
+    if (days < 1) { remaining = "today"; }
+    else if (days == 1) { remaining = "tomorrow"; }
+    else { remaining = "in "+days+" days"; }
+    outString = entry["title"]["$t"].replace(/(SIGNIFICANT: )([^-]+ - )(.*)/,"$3");
+    $('#comingup').append("<b>"+outString+"</b> "+remaining+"\n");
+  })
+}
+
 $(document).ready( function () {
   updateNagios();
   setInterval( updateNagios, 5000 );
   updateGoogleCalendar();
   setInterval( updateGoogleCalendar,900000);
+  updateSignificantEvent();
+  setInterval( updateSignificantEvent,900000);
 })
 
