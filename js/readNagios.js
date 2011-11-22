38218a410b1633d375d799e1dca24f6aa93abb85
Andrew Beresford <cs1ajb@brutus.shef.ac.uk>
2011-11-22 10:19:54 +0000

    Fix Dates on Safari

--- js/readNagios.js
+++ js/readNagios.js
@@ -17,7 +17,7 @@ function updateGoogleCalendar() {
 
 	$.getJSON('http://www.google.com/calendar/feeds/sheffield.ac.uk_5i8cmsq79qd3nm5spk9ftkol0k@group.calendar.google.com/public/full?alt=json-in-script&callback=?&orderby=starttime&max-results=5&singleevents=true&sortorder=ascending&futureevents=true', function (data) { 
 	  $.each(data["feed"]["entry"], function(value, data) { 
-	  calDate = new Date(data["gd$when"][0]["startTime"]);
+	  calDate = new Date(data["gd$when"][0]["startTime"]+'T00:00:00.000Z');
 	  dateString = calDate.getDate() + "/" + (calDate.getMonth()+1);
 	  event = data["title"]["$t"].replace(/([^,]+),?.*/,'$1');
 	  $('#calendar table').append("<tr><td class='date'>"+dateString+"</td><td class='event'>"+event+"</td></tr>\n");
